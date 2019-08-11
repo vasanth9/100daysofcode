b39013db703e6ac74ceaf2df06fd41a17ec39ed9
vasanth <cheepurupalli.vasanthkumar@gmail.com>
2019-08-11 12:03:05 +0530

    second version of day1

--- day1.cpp
+++ day1.cpp
@@ -4,8 +4,12 @@ int main()
 {
 //day zero of 100days of code;
 cout<<"trying to learn \"how  to use git\"";
-
-
+/*git config --global user.email "V@g.com"
+git add filename
+git status
+git commit -m 'message'
+git push
+*/
 
 
 
