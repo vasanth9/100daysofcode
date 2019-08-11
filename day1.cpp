e2bef570fdf6fa1c1b6c895eaa1a6cd17dd27e20
vasanth <cheepurupalli.vasanthkumar@gmail.com>
2019-08-11 12:15:44 +0530

    final version of day1

--- day1.cpp
+++ day1.cpp
@@ -4,7 +4,13 @@ int main()
 {
 //day zero of 100days of code;
 cout<<"trying to learn \"how  to use git\"";
-/*git config --global user.email "V@g.com"
+/*
+//username:vasanth9
+password:**************
+git clone copied url
+
+
+git config --global user.email "V@g.com"
 git add filename
 git status
 git commit -m 'message'
